c053a88794c9ecd6a09c70c2cbc189e22e66f10b
Chatelain Tanguy <tchatelain@enseirb-matmeca.fr>
2024-01-14 02:37:08 +0100

    Clean tkinterDisplay.py from unused code lines

--- sandbox/light/dmx_drivers/tkinterDisplay/tkinterDisplay.py
+++ sandbox/light/dmx_drivers/tkinterDisplay/tkinterDisplay.py
@@ -32,10 +32,9 @@
 
 from os import path
 import sys
-from platform import system
 from typing import List
 
-from pylibftdi import Device, Driver, LibraryMissingError
+from pylibftdi import Device
 
 from dmx.drivers import DMXDriver
 currentDir = path.dirname(path.abspath(__file__))
@@ -44,78 +43,8 @@ import tkinter_utils as tkutils
 
 DRIVER_PATH = path.abspath(path.dirname(__file__))
 
-if system() == "Linux":
-
-    from ctypes import cdll, c_long, byref, Structure
-
-    Driver._lib_search["libftdi"] = tuple([
-        path.join(DRIVER_PATH, "libftdi.so"),
-        path.join(DRIVER_PATH, "libftdi.so.1"),
-        path.join(DRIVER_PATH, "libftdi1.so")
-    ] + list(Driver._lib_search["libftdi"]))
-
-    _LIBC = cdll.LoadLibrary("libc.so.6")
-
-    class timespec(Structure):
-        """A timespec."""
-
-        _fields_ = [("tv_sec", c_long), ("tv_nsec", c_long)]
-
-    def wait_ms(milliseconds):
-        """Wait for a specified number of milliseconds."""
-        dummy = timespec()
-        sleeper = timespec()
-        sleeper.tv_sec = int(milliseconds / 1000)
-        sleeper.tv_nsec = (milliseconds % 1000) * 1000000
-        _LIBC.nanosleep(byref(sleeper), byref(dummy))
-
-    def wait_us(nanoseconds):
-        """Wait for a specified number of nanoseconds."""
-        dummy = timespec()
-        sleeper = timespec()
-        sleeper.tv_sec = int(nanoseconds / 1000000)
-        sleeper.tv_nsec = (nanoseconds % 1000) * 1000
-        _LIBC.nanosleep(byref(sleeper), byref(dummy))
-
-elif system() == "Windows":
-
-    from ctypes import wintypes, windll, byref
-
-    _WIN32 = windll.kernel32
-    _MS = 1000000
-    _INFINITE = 0xFFFFFFFF
-
-    def wait_ms(milliseconds):
-        """Wait for a specified number of milliseconds."""
-        wait_time = wintypes.LARGE_INTEGER(-(_MS * abs(milliseconds)))
-        timer_handle = _WIN32.CreateWaitableTimerW(None, True, None)
-        if timer_handle == 0:
-            raise Exception("CreateWaitableTimerW returned NULL")
-        if _WIN32.SetWaitableTimer(timer_handle, byref(wait_time), 0, None, None, False) == 0:
-            raise Exception("SetWaitableTimer returned 0")
-        _WIN32.WaitForSingleObject(timer_handle, _INFINITE)
-        _WIN32.CloseHandle(timer_handle)
-
-    def wait_us(nanoseconds):
-        """Wait for a specified number of nanoseconds."""
-        wait_time = wintypes.LARGE_INTEGER(-abs(nanoseconds))
-        timer_handle = _WIN32.CreateWaitableTimerW(None, True, None)
-        if timer_handle == 0:
-            raise Exception("CreateWaitableTimerW returned NULL")
-        if _WIN32.SetWaitableTimer(timer_handle, byref(wait_time), 0, None, None, False) == 0:
-            raise Exception("SetWaitableTimer returned 0")
-        _WIN32.WaitForSingleObject(timer_handle, _INFINITE)
-        _WIN32.CloseHandle(timer_handle)
-
-
 class TkinterDisplayer(Device, DMXDriver):
     """A DMX driver design for the University of York Serial-to-DMX usb adapter based on the FT232R."""
-
-    _BITS_8 = 8
-    _STOP_BITS_2 = 2
-    _PARITY_NONE = 0
-    _BREAK_OFF = 0
-    _BREAK_ON = 1
 
     def __init__(self, device_index=0):
         """Initialise the driver."""
@@ -129,7 +58,6 @@ class TkinterDisplayer(Device, DMXDriver):
         self.tkDisplayer.update()
         if (self.tkDisplayer.exitProgram):
             exit(130)
-        #wait_ms(30)
 
     def _set_break_on(self):
         pass
