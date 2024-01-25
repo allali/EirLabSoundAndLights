2eab4d47b69f39ad4db068c8e145ca97be559963
Chatelain Tanguy <tchatelain@enseirb-matmeca.fr>
2024-01-25 13:26:40 +0100

    Debugging thread queues

--- sandbox/light/dmx_wrapper/LightController.py
+++ sandbox/light/dmx_wrapper/LightController.py
@@ -3,24 +3,30 @@ import yaml
 from queue import Queue
 
 class LightController:
-    def __init__(self, file_name=None):
+    def __init__(self, nbLights):
         self.file_name = None
-        self.num_lights = 0
-        self.thread_list = []
-        self.thread_queues = []
+        self.num_lights = nbLights
+        self.thread_list = {}
+        self.thread_is_ready = {}
+        self.thread_is_finished = {}
+        self.thread_queues = {}
 
-        if file_name:
-            self.set_file(file_name)
+        #if file_name:
+        #    self.set_file(file_name)
 
     def _init_all_threads(self, data):
         for light_id in range(self.num_lights):
             self._init_thread(light_id, data)
+        while(len(self.thread_is_ready) != self.num_lights):
+            continue
+        while(False in [self.thread_is_ready[key] for key in self.thread_is_ready.keys()]):
+            continue
 
     def _init_thread(self, light_id, data):
         thread = threading.Thread(target=self._worker, args=(light_id,data))
         thread.start()
 
-        self.thread_list.append(thread)
+        self.thread_list[str(light_id)] = thread
 
     def set_file(self, file_name):
         data = self._read_yaml(file_name)
@@ -31,12 +37,11 @@ class LightController:
         for item in data:
             self._validate_yaml_item(item)
 
-        self.quit()
-
-        self.num_lights = len(data)
+        #self.num_lights = len(data)
         self.file_name = file_name
 
         self._init_all_threads(data)
+        #self.quit()
 
 
     def _read_yaml(self, file_name):
@@ -53,28 +58,45 @@ class LightController:
                 raise ValueError("Invalid YAML format. 'dt' must be a positive number greater than or equal to 25.")
     
     def _worker(self, light_id, data):
+        self.thread_is_ready[str(light_id)] = False
+        self.thread_is_finished[str(light_id)] = False
         print(f"Thread {light_id} started")
-        thread_queue = Queue(maxsize=1)
-        self.thread_queues.append(thread_queue)
+        thread_queue = Queue(maxsize=10)
+        self.thread_queues[str(light_id)] = thread_queue
 
         for light, id_chunk in enumerate(data):
-            if light == light_id:
+            if id_chunk['id'] == light_id:
                 for item in id_chunk.get('times', []):
                     thread_queue.put(item, block=True)
                     print(f"Thread {light_id} added block {item} to queue")
+                    self.thread_is_ready[str(light_id)] = True
+        self.thread_is_ready[str(light_id)] = True
+        self.thread_is_finished[str(light_id)] = True
 
 
     def get_block(self, light_id):
         if 0 <= light_id < self.num_lights:
-            return self.thread_queues[light_id].queue[0]
+            if (self.thread_queues[str(light_id)].qsize() > 0):     
+                return self.thread_queues[str(light_id)].queue[0]
+            elif self.thread_is_finished[str(light_id)]:
+                return {'dt': -1, 'red': 255, 'green': 255, 'blue': 255, 'white': 200, 'Tr': 0}
+            else:
+                while(self.thread_queues[str(light_id)].qsize() == 0):
+                    continue
+                return self.get_block(light_id)
+        else:
+            raise Exception(f"Wrong light id in get_block. Given id : {light_id}")
 
     def get_next_block(self, light_id):
         if 0 <= light_id < self.num_lights:
-            return self.thread_queues[light_id].queue[1]
+            if (self.thread_queues[str(light_id)].qsize() > 1): 
+                return self.thread_queues[str(light_id)].queue[1]
+            else:
+                return {'dt': -1, 'red': 255, 'green': 255, 'blue': 255, 'white': 200, 'Tr': 0}
 
     def remove_block(self, light_id):
         if 0 <= light_id < self.num_lights:
-            self.thread_queues[light_id].get()
+            self.thread_queues[str(light_id)].get()
 
     def empty_queues(self):
         for thread_queue in self.thread_queues:
@@ -82,15 +104,17 @@ class LightController:
                 thread_queue.get()
 
     def is_active(self):
-        for thread in self.thread_list:
-            if thread.is_alive():
+        for lightId in range(self.num_lights):
+            if self.thread_list[str(lightId)].is_alive() or self.thread_queues[str(lightId)].qsize() != 0:
                 return True
+            #print(lightId, self.thread_queues[str(lightId)].qsize())
+            
         return False
 
     def quit(self):
         #self.empty_queues()
-        for thread in self.thread_list:
-            thread.join()
+        for lightId in range(self.num_lights):
+            self.thread_list[str(lightId)].join()
 
 # # Example usage:
 # controller = LightController('sandbox/light/yamls/test.yaml')
--- sandbox/light/dmx_wrapper/main_loop.py
+++ sandbox/light/dmx_wrapper/main_loop.py
@@ -1,7 +1,11 @@
 import LightController
 from typing import List
+import sys
+sys.path.append("..")
+import os
 import dmx
 import time
+import LightController
 
 class Chronometer:
     def __init__(self):
@@ -21,52 +25,59 @@ class Chronometer:
             return timeEllapsed
         else:
             raise ValueError("Le chronomètre n'est pas démarré.")
+    
+    def slice_time(self):
+        if self.temps_debut is not None:
+            timeEllapsed = (time.time() - self.temps_debut) * 1000
+            self.temps_debut = time.time()
+            return timeEllapsed
+        else:
+            raise ValueError("Le chronomètre n'est pas démarré.")
 
 
 def get_transition_0(block)->tuple:
     return block['red'], block['green'], block['blue'], block['white']
 
 def get_transition_1(firstBlock, secondBlock, t:int)->tuple:
-    ratio = t/firstBlock['dt']
-    r:int = int(firstBlock['red'] + (firstBlock['red'] - secondBlock['red']) * ratio)
-    g:int = int(firstBlock['green'] + (firstBlock['green'] - secondBlock['green']) * ratio)
-    b:int = int(firstBlock['blue'] + (firstBlock['blue'] - secondBlock['blue']) * ratio)
-    w:int = int(firstBlock['white'] + (firstBlock['white'] - secondBlock['white']) * ratio)
+    ratio = (1. * t)/firstBlock['dt']
+    r:int = int(firstBlock['red'] + (secondBlock['red'] - firstBlock['red']) * ratio)
+    g:int = int(firstBlock['green'] + (secondBlock['green'] - firstBlock['green']) * ratio)
+    b:int = int(firstBlock['blue'] + (secondBlock['blue'] - firstBlock['blue']) * ratio)
+    w:int = int(firstBlock['white'] + (secondBlock['white'] - firstBlock['white']) * ratio)
     return r, g, b, w
 
 universe = dmx.DMXUniverse()
 interface = dmx.DMXInterface("TkinterDisplayer")
 nbLights:int = 54
 timeStep:int = 30
-fileName = "'sandbox/light/yamls/test.yaml'"
+fileName = r"../yamls/snake.yml"
 
 
 times:List[int] = [0]*nbLights
-lights = [dmx.DMXLight4Slot(address=dmx.light.light_map[i]) for i in range()]
+lights = [dmx.DMXLight4Slot(address=dmx.light.light_map[i]) for i in range(nbLights)]
 for light in lights:
     universe.add_light(light)
 
-thrd = LightController()
+thrd = LightController.LightController(nbLights)
 thrd.set_file(fileName)
 chrono = Chronometer()
 chrono.start()
 
 while(thrd.is_active()):
-    currentTime = chrono.get_time()
+    currentTime = chrono.slice_time()
     for lightId in range(nbLights):
         times[lightId] = times[lightId] + currentTime
         currentBlock = thrd.get_block(lightId)
 
-        if currentBlock['dt'] >= times[lightId]:
+        if currentBlock['dt'] <= times[lightId] and currentBlock['dt'] != -1:
             times[lightId] %= timeStep
             thrd.remove_block(lightId)
             currentBlock = thrd.get_block(lightId)
-
         if currentBlock['Tr'] == 0:
             r,g,b,w = get_transition_0(currentBlock)
             lights[lightId].set_colour(dmx.Color(r,g,b,w))
-        elif currentTime['Tr'] == 1:
-            r,g,b,w = get_transition_1(currentBlock, thrd.get_next_block(lightId), currentTime)
+        elif currentBlock['Tr'] == 1:
+            r,g,b,w = get_transition_1(currentBlock, thrd.get_next_block(lightId), times[lightId])
             lights[lightId].set_colour(dmx.Color(r,g,b,w))      
         
 
