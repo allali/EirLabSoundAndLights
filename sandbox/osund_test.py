2326921bc6a2a99b1ab747f82020a9b02c5a8986
ROBERT Joachim <Joachim.Robert@bordeaux-inp.fr>
2024-03-07 11:50:13 +0100

    added rms calculation to the AudioPlayer file and create a onyl sound test

---
+++ sandbox/osund_test.py
@@ -0,0 +1,17 @@
+import time
+import sys
+
+sys.path.append("./sound")
+
+from play_file_func import AudioPlayer
+
+buffsize = 20 
+audioFile = "./sound/10tracks_studio/universal_10_pistes.wav"
+clientName = "test_sound"
+
+audioPlayer = AudioPlayer(clientName, buffsize)
+
+audioPlayer.load_file(audioFile)
+
+audioPlayer.start()
+
