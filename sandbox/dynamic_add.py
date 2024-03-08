e0e78c66ef909e3ae79ede91a795d785ba3d4888
MARTINEZ JEAN-BAPTISTE <jeamartinez@bordeaux-inp.fr>
2024-03-08 13:10:58 +0100

    dynamic add

--- sandbox/dynamic_add.py
+++ sandbox/dynamic_add.py
@@ -9,8 +9,9 @@ sys.path.append("./light/dmx_wrapper")
 sys.path.append("./light")
 sys.path.append("./sound")
 import bindings
+import config as cf
 
-from Player import YamlReader, Player
+from New_player import YamlReader, Player
 
 def detect_keypress():
     fd = sys.stdin.fileno()
@@ -27,19 +28,46 @@ nbLights = 54
 interfaceName = "FT232R" # "FT232R" "TkinterDisplayer"
 player = Player(54, interfaceName)
 yr = YamlReader()
-yr.load_file(r"./light/yamls/test.yaml", player, 0, False)
+yr.load_file(r"./light/yamls/test.yaml", player)
 #yr.load_file(r"./light/yamls/test.yaml", player, 1000)
 player.start()
+
+current_light = {'x':None, 'y':None}
 
 while True:
     key = detect_keypress()
     print(f"Touche pressée : {key}")
 
-    if key == 'q':
-        break
+    if key == 'z' and current_light['x'] is not None and current_light['y'] < cf.number_of_columns -1:
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [200,200,200,200], 0, 0)
+        current_light['y'] += 1 
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [255,0,0,0], 0, 0)
+
+    if key == 'q' and current_light['x'] is not None and current_light['x'] < cf.number_of_rows -1:
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [200,200,200,200], 0, 0)
+        current_light['x'] += 1
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [255,0,0,0], 0, 0)
+
+    if key == 's' and current_light['x'] is not None and current_light['y'] > 0:
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [200,200,200,200], 0, 0)
+        current_light['y'] -= 1
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [255,0,0,0], 0, 0)
+
+    if key == 'd' and current_light['x'] is not None and current_light['x'] > 0:
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [200,200,200,200], 0, 0)
+        current_light['x'] -= 1
+        player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [255,0,0,0], 0, 0)
 
     if key == 'r':
-        player.add_new_set()
-        player.add(random.randint(0,53), 500, [255,0,0,0], 0, 200, True)
+        if current_light['x'] is not None:
+            player.add(current_light['x'] * cf.number_of_columns + current_light['y'], player.timer.get_time() + 540, [200,200,200,200], 0, 0)
 
-    player.quit()
+        point = random.randint(0,53)
+        current_light['x'] = int(point / cf.number_of_columns)
+        current_light['y'] = int(point % cf.number_of_columns)
+        player.add(point, player.timer.get_time() + 540, [255,0,0,0], 0, 0)
+
+    if key == 'p':
+        break
+
+player.quit()
